28997ddda0d463899b8f41082eb68f6c0af20725
Razvi <drawwithskull@gmail.com>
2022-10-19 09:36:28 +0300

    Add project files.

---
+++ Proiect facultate/Program.cs
@@ -0,0 +1,14 @@
+﻿using Proiect_facultate.src;
+
+namespace Main
+{
+    class App
+    {
+        static void Main(string[] args)
+        {
+            Exercise1 e1 = new();
+            e1.AddInts();
+            e1.PrintList();
+        }
+    }
+}
---
+++ Proiect facultate/src/Exercise1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proiect_facultate.src
+{
+    public class Exercise1
+    {
+        private List<int> arrayOfInts;
+        public Exercise1()
+        {
+            arrayOfInts = new List<int>();
+        }
+        public void AddInts()
+        {
+            int number;
+            number = int.Parse(Console.ReadLine());
+            while (number % 100 != 0)
+            {
+                arrayOfInts.Add(number);
+                number = int.Parse(Console.ReadLine());
+            }
+        }
+        public void PrintList()
+        {
+            foreach(int number in arrayOfInts)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
